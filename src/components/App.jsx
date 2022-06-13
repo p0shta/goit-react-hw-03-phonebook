58a462479ca91ae5a7ce980f2f62dde932e96e02
Maksym Pshenychnyi <maksim_pshenichniy@mail.ru>
2022-06-13 15:13:23 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,7 @@ export class App extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if (prevState !== this.state.contacts) {
+        if (prevState.contacts !== this.state.contacts) {
             localStorage.setItem(
                 'contacts',
                 JSON.stringify(this.state.contacts)
